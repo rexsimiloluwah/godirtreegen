4da0d12ec0501f0f1c4c6c5ab9ca1f6d597c0c87
rexsimiloluwah <rexsimiloluwa@gmail.com>
2022-04-07 18:33:08 +0100

    Brew formula update for godirtreegen version v0.1.6

--- godirtreegen.rb
+++ godirtreegen.rb
@@ -5,11 +5,11 @@
 class Godirtreegen < Formula
   desc ""
   homepage "https://github.com/rexsimiloluwah/godirtreegen"
-  version "0.1.5"
+  version "0.1.6"
 
   on_macos do
-    url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.5/godirtreegen_0.1.5_darwin_all.tar.gz"
-    sha256 "353fd7e872081311180155c3deae370648796a6be536cde46d8393dbe5d56e90"
+    url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.6/godirtreegen_0.1.6_darwin_all.tar.gz"
+    sha256 "60c43f5a073b57c6caac5159a94cbb1b2e23e5abceb5dd0264a5b34156e0e954"
 
     def install
       bin.install "godirtreegen"
@@ -17,25 +17,25 @@ class Godirtreegen < Formula
   end
 
   on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.6/godirtreegen_0.1.6_linux_amd64.tar.gz"
+      sha256 "93930608d7dc3b6bde01f9fd7954c6387e1fcf9139ecf336cc19be49f7b2626a"
+
+      def install
+        bin.install "godirtreegen"
+      end
+    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.5/godirtreegen_0.1.5_linux_arm64.tar.gz"
-      sha256 "1faa1841af0a3572a77d6173f8b59615f46ec4d83a565e33aa39c57ca948fb62"
+      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.6/godirtreegen_0.1.6_linux_arm64.tar.gz"
+      sha256 "26c29d266dd723062f3322d882e9a708b65cc9115560adb60a8412d0b4ffeac8"
 
       def install
         bin.install "godirtreegen"
       end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.5/godirtreegen_0.1.5_linux_armv6.tar.gz"
-      sha256 "ad99315d6e2c81089046793a5f4cb011455f6b1c02be52d0f57828196dfa4084"
-
-      def install
-        bin.install "godirtreegen"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.5/godirtreegen_0.1.5_linux_amd64.tar.gz"
-      sha256 "8b3fa5a9a1ae46e529071cf9244edf30c339583984ba48eacbf57e57bac6dbb2"
+      url "https://github.com/rexsimiloluwah/godirtreegen/releases/download/v0.1.6/godirtreegen_0.1.6_linux_armv6.tar.gz"
+      sha256 "11c91d1454163f42093ade6d4c6b52791012198727cba4855afc7785a50ee93a"
 
       def install
         bin.install "godirtreegen"
